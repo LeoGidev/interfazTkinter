7a22caa2cafeca5cfcba1d365642cbcd105418f6
Leo Gimenez <leogimenez.dev@gmail.com>
2024-10-14 14:29:03 -0300

    se trabaja init

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ class FootballApp:
         self.root.title("Tabla de posiciones")
         self.label = tk.Label(root, text="Generador de Tabla de Posiciones")
         self.label.pack(pady=10)
+        self.load_button = tk.Button(root, text="Cargar Archivo de Google Drive", command=self.load_file)
+        self.load_button.pack(pady=5)
